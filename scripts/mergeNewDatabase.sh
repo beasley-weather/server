f00bdcd087f7abb55a5f5ab2a2457b72828b52e3
Kyle Arscott <arscotkm@mcmaster.ca>
2017-09-19 11:18:17 +1000

    Modify mergerNewDatabase.sh which now successfully merge two databases created using data_generator.py

--- scripts/mergeNewDatabase.sh
+++ scripts/mergeNewDatabase.sh
@@ -2,15 +2,23 @@
 
 echo -e "\nAbout to merge master.db and newPush.sb..."
 
+SERVER_USER=kyle
+STATION_ROOT_DIR=/home/$SERVER_USER/beasley-weather-station
+SERVER_SCRIPTS_DIR=$STATION_ROOT_DIR/server/scripts
+SERVER_DB_DIR=$STATION_ROOT_DIR/server/db
+DATA_GENERATOR_SCRIPT=$STATION_ROOT_DIR/data-generator/data_generator.py
+
 # Example how to run sqlite3 commands from a script
 # echo "select * from archive;" > sqlite3Script.sql
 # sqlite3 sqlite3database.db < sqlite3Script.sql
 
 # merge master.db + newPush.db = master.db
 
+cd $SERVER_DB_DIR
+
 # create 2 test databases
-../data-generator/data_generator.py -p 24 -t 0 ./master.db temp sin
-../data-generator/data_generator.py -p 24 -t 25 ./newPush.db wind square
+$DATA_GENERATOR_SCRIPT -p 24 -t 0 ./master.db temp sin
+$DATA_GENERATOR_SCRIPT -p 24 -t 25 ./newPush.db wind square
 
 # merge two databases using an SQL script
 sqlite3 master.db < sqlite3ScriptMerge.sql
@@ -27,7 +35,7 @@ sqlite3 master.db < sqlite3ScriptMerge.sql
 ## Directory in which the database files are located
 #        SQLITE_ROOT = /var/lib/weewx
 # move master database to the location specified in weewx.conf
-cp -v master.db /var/lib/weewx/weewx.sdb
+cp -v $SERVER_DB_DIR/master.db /var/lib/weewx/weewx.sdb
 
 # use the new weewx.sdb file and generate a new "report" (index.html)
 # The [[FTP]] tags in the weewx.conf are setup so that the newly
